9d491753e735fe517a737347c622fded24dad1dc
Ederson Davi <silva.edersonqueiroz@gmail.com>
2020-04-29 19:55:58 -0300

    Create function run

--- src/script.js
+++ src/script.js
@@ -1,13 +1,48 @@
 //GET ELEMENTS HTML
 //img night
-var nigth =  document.querySelector("#night")
+let nigth =  document.querySelector("#night")
 
 //Buttons
-var btn_start =  document.querySelector("#start")
-var btn_reset =  document.querySelector("#reset")
-var btn_lap =  document.querySelector("#lap")
-var btn_pause =  document.querySelector("#pause")
-var btn_return =  document.querySelector("#return")
+let btn_start =  document.querySelector("#start")
+let btn_reset =  document.querySelector("#reset")
+let btn_lap =  document.querySelector("#lap")
+let btn_pause =  document.querySelector("#pause")
+let btn_return =  document.querySelector("#return")
+
+//Displays 
+let lbl_miliSec = document.querySelector("#miliSec")
+let lbl_sec = document.querySelector("#sec")
+let lbl_min = document.querySelector("#min")
+let lbl_hr = document.querySelector("#hr")
+
+//Function mode nigth
+nigth.addEventListener('click', ()=> {
+    let body = document.querySelector("body")
+    let header = document.querySelector("header")
+    let section = document.querySelector("section")
+    let buttons = document.querySelectorAll("button")
+
+    if(body.classList.contains("bodyLight")){
+
+        body.classList.replace("bodyLight","bodyDark")
+        header.classList.replace("headerLight","headerDark")
+        section.classList.replace("sectionLight","sectionDark")
+
+        for(let button of buttons){
+            button.classList.replace("buttonLight","buttonDark")
+        }
+
+    }else{
+        body.classList.replace("bodyDark","bodyLight")
+        header.classList.replace("headerDark","headerLight")
+        section.classList.replace("sectionDark","sectionLight")
+
+        for(let button of buttons){
+            button.classList.replace("buttonDark","buttonLight")
+        }
+    }
+
+})
 
 //Function hide buttons
 function HideButtons(){
@@ -35,6 +70,7 @@ function HideButtons(){
         btn_return.style.display = "inline"
         btn_reset.style.display = "inline"
     }
+    
 }
 
 function RestartButtons(){
@@ -48,37 +84,20 @@ function RestartButtons(){
 
 }
 
-btn_start.addEventListener('click',HideButtons)
+function Run() {
+    let numMiliSec = 0;
+
+    setInterval(() => {
+        lbl_miliSec.innerHTML =  numMiliSec
+        numMiliSec++
+    }, 100);
+    
+}
+
+//Call Functions
+btn_start.addEventListener('click',()=>{HideButtons(); Run()})
 btn_pause.addEventListener('click',HideButtons)
 btn_return.addEventListener('click',HideButtons)
 btn_reset.addEventListener('click',RestartButtons)
 
 
-//Function mode nigth
-nigth.addEventListener('click', ()=> {
-    var body = document.querySelector("body")
-    var header = document.querySelector("header")
-    var section = document.querySelector("section")
-    var buttons = document.querySelectorAll("button")
-
-    if(body.classList.contains("bodyLight")){
-
-        body.classList.replace("bodyLight","bodyDark")
-        header.classList.replace("headerLight","headerDark")
-        section.classList.replace("sectionLight","sectionDark")
-
-        for(let button of buttons){
-            button.classList.replace("buttonLight","buttonDark")
-        }
-
-    }else{
-        body.classList.replace("bodyDark","bodyLight")
-        header.classList.replace("headerDark","headerLight")
-        section.classList.replace("sectionDark","sectionLight")
-
-        for(let button of buttons){
-            button.classList.replace("buttonDark","buttonLight")
-        }
-    }
-
-})
