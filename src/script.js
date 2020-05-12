c13ecd6d7df7e660e63bf26ccde276359a34907b
Ederson Davi <silva.edersonqueiroz@gmail.com>
2020-05-12 19:27:24 -0300

    function reset and styles in laps

--- src/script.js
+++ src/script.js
@@ -14,18 +14,20 @@ let lbl_miliSec = document.querySelector("#miliSec")
 let lbl_sec = document.querySelector("#sec")
 let lbl_min = document.querySelector("#min")
 let lbl_hr = document.querySelector("#hr")
+let lapArea = document.querySelector('.laps')
 
 //Var for interval
 let interval;
 
-//Function mode nigth
+//Function style dark
 nigth.addEventListener('click', () => {
   let body = document.querySelector("body")
-  var header = document.querySelector("header")
-  var section = document.querySelector("section")
-  var buttons = document.querySelectorAll("button")
+  let header = document.querySelector("header")
+  let section = document.querySelector("section")
+  let buttons = document.querySelectorAll("button")
+  let divLaps = document.querySelector(".laps")
 
-  var current = 'Light',
+  let current = 'Light',
     replaceable = 'Dark';
 
   if (body.classList.contains("bodyDark")) {
@@ -36,6 +38,7 @@ nigth.addEventListener('click', () => {
   body.classList.replace("body" + current, "body" + replaceable)
   header.classList.replace("header" + current, "header" + replaceable)
   section.classList.replace("section" + current, "section" + replaceable)
+  divLaps.classList.replace("laps" + current, "laps" + replaceable)
 
   for (let button of buttons) {
     button.classList.replace("button" + current, "button" + replaceable)
@@ -88,54 +91,71 @@ let contSec = 00
 let contmin = 00
 let conthora = 00
 
-//Call Functions
-btn_start.addEventListener('click', () => {
-  HideButtons();
+//index laps
+let contLap = 1
 
-
+function initCount() {
   function count() {
     if (contMili == 100) {
+      lbl_sec.innerHTML = contSec < 9 ? "0" + ++contSec : ++contSec
       contMili = 0
-      lbl_sec.innerHTML = contSec < 10 ? "0" + contSec++ : contSec++
-      if (contSec == 61) {
+      if (contSec == 60) {
         lbl_sec.innerHTML = "0" + 0
         lbl_min.style.display = "inline"
-        lbl_min.innerHTML = contmin < 10 ? "0" + contmin++ + ":" : contmin++ + ":"
+        lbl_min.innerHTML = contmin < 9 ? "0" + ++contmin + ":" : ++contmin + ":"
         contSec = 1
-        if (contmin == 61) {
+        if (contmin == 60) {
           lbl_min.innerHTML = "0:" + 0
           lbl_hr.style.display = "inline"
-          lbl_hr.innerHTML = conthora < 10 ? "0" + conthora++ + ":" : conthora++ + ":"
+          lbl_hr.innerHTML = conthora < 9 ? "0" + ++conthora + ":" : ++conthora + ":"
           contmin = 1
         }
       }
     }
-    lbl_miliSec.innerHTML = "." + contMili++
+    lbl_miliSec.innerHTML = "." + ++contMili
   }
   interval = setInterval(count, 10)
+}
 
+//Function reset values
+function clearAndResetCount() {
+  contMili = 00
+  contSec = 00
+  contmin = 00
+  conthora = 00
+  lbl_miliSec.innerHTML = '.0' + 0
+  lbl_sec.innerHTML = '0' + 0
+  lbl_min.innerHTML = 0
+  lbl_hr.innerHTML = 0
+  clearInterval(interval)
+  lapArea.innerHTML = ''
+  contLap = 1
+  lbl_min.style.display = "none"
+  lbl_hr.style.display = "none"
+}
+
+//Call Functions
+btn_start.addEventListener('click', () => {
+  HideButtons();
+  initCount()
 })
 
 btn_pause.addEventListener('click', () => {
-
   HideButtons()
-
   clearInterval(interval)
-
 })
 
 btn_return.addEventListener('click', () => {
   HideButtons()
-
+  initCount()
 })
 
-btn_reset.addEventListener('click', RestartButtons)
-
-let contLap = 1
+btn_reset.addEventListener('click', () => {
+  clearAndResetCount()
+  RestartButtons()
+})
 
 btn_lap.addEventListener('click', () => {
-  let lapArea = document.querySelector('.laps')
-
   lapArea.innerHTML += `<div>Lap ${contLap < 10 ? "0" + contLap : contLap} - ${conthora < 10 ? "0" + conthora : conthora}:${contmin < 10 ? "0" + contmin : contmin}:${contSec < 10 ? "0" + contSec : contSec}.<span style=" font-size: 0.5em">${contMili}</span></div>`
   contLap++
 })
